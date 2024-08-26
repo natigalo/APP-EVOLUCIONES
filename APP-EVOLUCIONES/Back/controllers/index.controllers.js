9832eaf9d034a784bab19a31f99af2d7dba13ee3
CarlosCortesLV <carlos21nik@gmail.com>
2024-08-25 19:59:48 -0500

    Metodos eliminar y editar

--- APP-EVOLUCIONES/Back/controllers/index.controllers.js
+++ APP-EVOLUCIONES/Back/controllers/index.controllers.js
@@ -114,4 +114,162 @@ const createHabilidadActividad = async (req, res) => {
     res.status(500).send(error);
   }
 };
-module.exports = { getIntegracionTablas, getObjetivos, getHabiidades, getActividades, createHabilidad, createObjetivo, createActividad, createObjetivoHabilidad, createHabilidadActividad };
+// Funciones para editar
+const updateObjetivo = async (req, res) => {
+  const { id, name } = req.body; // Obtener los datos del cuerpo de la solicitud
+  try {
+    const response = await pool.query('UPDATE public."OBJETIVOS" SET "NAME_OBJ" = $1 WHERE "ID_OBJ" = $2 RETURNING *',
+      [name, id]);
+    res.status(200).send(response.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const updateHabilidad = async (req, res) => {
+  const { id, name } = req.body; // Obtener los datos del cuerpo de la solicitud
+  try {
+    const response = await pool.query('UPDATE public."HABILIDADES" SET "NAME_HAB" = $1 WHERE "ID_HAB" = $2 RETURNING *',
+      [name, id]);
+    res.status(200).send(response.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const updateActividad = async (req, res) => {
+  const { id, name } = req.body; // Obtener los datos del cuerpo de la solicitud
+  try {
+    const response = await pool.query('UPDATE public."ACTIVIDADES" SET "NAME_ACT" = $1 WHERE "ID_ACT" = $2 RETURNING *',
+      [name, id]);
+    res.status(200).send(response.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const updateObjetivoHabilidad = async (req, res) => {
+  const { id_obj, id_hab } = req.body; // Obtener los datos del cuerpo de la solicitud
+  try {
+    const response = await pool.query('UPDATE public."OBJETIVO-HABILIDAD" SET "ID_HAB" = $1 WHERE "ID_OBJ" = $2 RETURNING *',
+      [id_hab, id_obj]);
+    res.status(200).send(response.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const updateHabilidadActividad = async (req, res) => {
+  const { id_hab, id_act } = req.body; // Obtener los datos del cuerpo de la solicitud
+  try {
+    const response = await pool.query('UPDATE public."HABILIDADES-ACTIVIDADES" SET "ID_ACT2" = $1 WHERE "ID_HAB2" = $2 RETURNING *',
+      [id_act, id_hab]);
+    res.status(200).send(response.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+// Funciones para eliminar
+const deleteObjetivo = async (req, res) => {
+  const { id } = req.params; // Obtener el ID de los parámetros de la solicitud
+  try {
+    const response = await pool.query('DELETE FROM public."OBJETIVOS" WHERE "ID_OBJ" = $1 RETURNING *', [id]);
+    if (response.rowCount === 0) {
+      res.status(404).send('Objetivo no encontrado');
+    } else {
+      res.status(200).send(response.rows[0]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const deleteHabilidad = async (req, res) => {
+  const { id } = req.params; // Obtener el ID de los parámetros de la solicitud
+  try {
+    const response = await pool.query('DELETE FROM public."HABILIDADES" WHERE "ID_HAB" = $1 RETURNING *', [id]);
+    if (response.rowCount === 0) {
+      res.status(404).send('Habilidad no encontrada');
+    } else {
+      res.status(200).send(response.rows[0]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const deleteActividad = async (req, res) => {
+  const { id } = req.params; // Obtener el ID de los parámetros de la solicitud
+  try {
+    const response = await pool.query('DELETE FROM public."ACTIVIDADES" WHERE "ID_ACT" = $1 RETURNING *', [id]);
+    if (response.rowCount === 0) {
+      res.status(404).send('Actividad no encontrada');
+    } else {
+      res.status(200).send(response.rows[0]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const deleteObjetivoHabilidad = async (req, res) => {
+  const { id_obj, id_hab } = req.params; // Obtener los IDs de los parámetros de la solicitud
+  try {
+    const response = await pool.query('DELETE FROM public."OBJETIVO-HABILIDAD" WHERE "ID_OBJ" = $1 AND "ID_HAB" = $2 RETURNING *', [id_obj, id_hab]);
+    if (response.rowCount === 0) {
+      res.status(404).send('Relación Objetivo-Habilidad no encontrada');
+    } else {
+      res.status(200).send(response.rows[0]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+const deleteHabilidadActividad = async (req, res) => {
+  const { id_hab, id_act } = req.params; // Obtener los IDs de los parámetros de la solicitud
+  try {
+    const response = await pool.query('DELETE FROM public."HABILIDADES-ACTIVIDADES" WHERE "ID_HAB2" = $1 AND "ID_ACT2" = $2 RETURNING *', [id_hab, id_act]);
+    if (response.rowCount === 0) {
+      res.status(404).send('Relación Habilidad-Actividad no encontrada');
+    } else {
+      res.status(200).send(response.rows[0]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+
+module.exports = { 
+  getIntegracionTablas, 
+  getObjetivos, 
+  getHabiidades, 
+  getActividades, 
+  createHabilidad, 
+  createObjetivo, 
+  createActividad, 
+  createObjetivoHabilidad, 
+  createHabilidadActividad,
+  updateObjetivo,
+  updateHabilidad,
+  updateActividad,
+  updateObjetivoHabilidad,
+  updateHabilidadActividad,
+  deleteObjetivo,
+  deleteHabilidad,
+  deleteActividad,
+  deleteObjetivoHabilidad,
+  deleteHabilidadActividad 
+};
--- APP-EVOLUCIONES/Back/routes/index.js
+++ APP-EVOLUCIONES/Back/routes/index.js
@@ -1,17 +1,54 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getIntegracionTablas, getObjetivos, getHabiidades, getActividades, createHabilidad, createObjetivo, createActividad, createObjetivoHabilidad, createHabilidadActividad } = require('../controllers/index.controllers.js');
-
+const { 
+    getIntegracionTablas, 
+    getObjetivos, 
+    getHabiidades, 
+    getActividades, 
+    createHabilidad, 
+    createObjetivo, 
+    createActividad, 
+    createObjetivoHabilidad, 
+    createHabilidadActividad, 
+    updateObjetivo, 
+    updateHabilidad, 
+    updateActividad, 
+    updateObjetivoHabilidad, 
+    updateHabilidadActividad, 
+    deleteObjetivo, 
+    deleteHabilidad, 
+    deleteActividad, 
+    deleteObjetivoHabilidad, 
+    deleteHabilidadActividad 
+  } = require('../controllers/index.controllers.js');
+  
+// Rutas para obtener datos
 router.get('/integracion', getIntegracionTablas);
 router.get('/objetivos', getObjetivos);
 router.get('/habilidades', getHabiidades);
 router.get('/actividades', getActividades);
 
+// Rutas para crear datos
 router.post('/objetivo', createObjetivo);
 router.post('/habilidad', createHabilidad);
 router.post('/actividad', createActividad);
 router.post('/obj_habi', createObjetivoHabilidad);
 router.post('/hab_act', createHabilidadActividad);
+
+// Rutas para editar datos
+router.put('/objetivo/:id', updateObjetivo);
+router.put('/habilidad/:id', updateHabilidad);
+router.put('/actividad/:id', updateActividad);
+router.put('/obj_habi/:id_obj/:id_hab', updateObjetivoHabilidad);
+router.put('/hab_act/:id_hab/:id_act', updateHabilidadActividad);
+
+// Rutas para eliminar datos
+router.delete('/objetivo/:id', deleteObjetivo);
+router.delete('/habilidad/:id', deleteHabilidad);
+router.delete('/actividad/:id', deleteActividad);
+router.delete('/obj_habi/:id_obj/:id_hab', deleteObjetivoHabilidad);
+router.delete('/hab_act/:id_hab/:id_act', deleteHabilidadActividad);
+
 
 module.exports = router;
