a485fb561b654eac3bce4eac24165d0301f71299
CarlosCortesLV <carlos21nik@gmail.com>
2024-09-14 13:10:51 -0500

    Template y modificaciones

--- APP-EVOLUCIONES/frontend/src/main.jsx
+++ APP-EVOLUCIONES/frontend/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+
+// layouts
+// import Admin from "layouts/Admin.js";
 import App from './App.jsx'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Switch>
+          {/* add routes with layouts */}
+          <Route path="/admin" component={App} />
+          {/* <Route path="/auth" component={Auth} /> */}
+          {/* add routes without layouts */}
+          {/* <Route path="/landing" exact component={Landing} />
+          <Route path="/profile" exact component={Profile} />
+          <Route path="/" exact component={Index} /> */}
+          {/* add redirect for first page */}
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>,
   </React.StrictMode>,
 )
